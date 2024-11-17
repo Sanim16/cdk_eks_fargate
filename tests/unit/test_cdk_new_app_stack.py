fa2ddedb9b1b558a0905c593b15545c384c7ba39
Momoh Sani Musa <sanim16@yahoo.com>
2024-11-17 01:43:35 +0100

    update README

--- tests/unit/test_cdk_new_app_stack.py
+++ tests/unit/test_cdk_new_app_stack.py
@@ -3,19 +3,36 @@ import aws_cdk.assertions as assertions
 from cdk_new_app.cdk_new_app_stack import CdkNewAppStack
 
 
-def test_sqs_queue_created():
+def test_ec2_vpc_created():
     app = core.App()
     stack = CdkNewAppStack(app, "cdk-new-app")
     template = assertions.Template.from_stack(stack)
 
-    template.has_resource_properties("AWS::SQS::Queue", {
-        "VisibilityTimeout": 300
+    template.has_resource_properties("AWS::EC2::VPC", {
+        "CidrBlock": "10.0.0.0/16"
     })
 
-
-def test_sns_topic_created():
+def test_eks_fargate_created():
     app = core.App()
     stack = CdkNewAppStack(app, "cdk-new-app")
     template = assertions.Template.from_stack(stack)
 
-    template.resource_count_is("AWS::SNS::Topic", 1)
+    template.has_resource_properties("Custom::AWSCDK-EKS-Cluster", {
+        "Config": {"name": "max-cluster"}
+    })
+
+def test_eks_fargate_profile():
+    app = core.App()
+    stack = CdkNewAppStack(app, "cdk-new-app")
+    template = assertions.Template.from_stack(stack)
+
+    template.has_resource_properties("Custom::AWSCDK-EKS-FargateProfile", {
+        "Config": {"selectors": [{"namespace": "maxapp"}]}
+    })
+
+def test_eks_fargate_profile_created():
+    app = core.App()
+    stack = CdkNewAppStack(app, "cdk-new-app")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("Custom::AWSCDK-EKS-FargateProfile", 2)
